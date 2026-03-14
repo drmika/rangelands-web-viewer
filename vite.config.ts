826cfb9cd5090f77fb53d6c7a77d9196db23524a
Max Jones <14077947+maxrjones@users.noreply.github.com>
2026-03-13 22:04:15 -0400

    Add github workflow

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/boreal-web-viewer/",
   worker: { format: "es" },
   server: {
     port: 3000,
